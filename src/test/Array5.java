138afdeafdc8b484ac522883d7c346c1ac7e37f6
Beatriz_Calzadilla <betty071194@gmail.com>
2025-11-18 12:25:51 +0100

    leerEdades working
    the function works, corrects and saves the values

---
+++ src/test/Array5.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package test;
+
+import java.util.Scanner;
+
+/**
+ * 
+ */
+public class Array5 {
+
+	private static Scanner teclado = new Scanner(System.in);
+	private static int listaEdades[] = new int[20];
+ 	
+	public static void main(String[] args) {
+		leerEdad();
+		for(int i=0; i<listaEdades.length; i++) {
+			System.out.println("--------------------");
+			System.out.println(i+1+"- Edad: "+listaEdades[i]);
+		}
+
+	}
+	private static int[] leerEdad() {
+		
+		int valorEdad=0;
+		int cantidad= 0;
+		
+		do{
+			boolean correcto= false;
+			while(!correcto){
+				System.out.println("\nIntroduzca un numero entre 18 y 35: ");
+				String edad= teclado.nextLine();
+				
+				try {
+					valorEdad = Integer.parseInt(edad);
+					
+					if (valorEdad<18 || valorEdad>35) {
+						System.out.println("\nERROR: Debe introducir una edad entre 18 y 35");
+					} else {
+						correcto=true;
+						listaEdades[cantidad]=valorEdad;
+						cantidad++;
+						System.out.println("\n ++++++++++++CORRECTO++++++++++"
+								+ "\nSe ha guardado el valor de edad: "+valorEdad+" y ha guardado "+cantidad+" edades");
+						
+					}
+				} catch (NumberFormatException e){
+					System.out.println("\nERROR: Debe introducir un numero");
+				}
+			}
+		} while(cantidad<20);
+		return listaEdades;
+	}
+}
