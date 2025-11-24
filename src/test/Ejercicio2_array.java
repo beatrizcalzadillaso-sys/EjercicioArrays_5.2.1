5d457c0da1f57c2081cbaf81fe86261d96cda54e
beatrizcalzadillaso <beatriz.calzadillaso@elorrieta-errekamari.com>
2025-11-24 21:02:55 +0100

    finished 2
    completed and working

--- src/test/Ejercicio2_array.java
+++ src/test/Ejercicio2_array.java
@@ -13,12 +13,14 @@ public class Ejercicio2_array {
 	//private static Scanner key = new Scanner(System.in);
 	private static int numbers[] = new int[20];
 	private static int suma = 0;
+	private static int producto=1;
 	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		cargarTabla();
 		sumaPares(numbers);
+		prodImpares(numbers);
 		imprimirDatos(numbers);
 
 	}
@@ -44,8 +46,16 @@ public class Ejercicio2_array {
 		for (int i=1; i<lista.length; i= i+2) {
 			suma = suma + lista[i];
 		}
-		System.out.println("La suma de los pares es "+suma);
+		//System.out.println("La suma de los pares es "+suma);
 		return suma;
+	}
+	
+	private static int prodImpares(int [] lista) {
+		for (int i=0; i<lista.length-1; i= i+2) {
+			producto = producto * lista[i];
+		}
+		
+		return producto;
 	}
 	
 	private static void imprimirDatos(int[] lista) {
@@ -54,6 +64,9 @@ public class Ejercicio2_array {
 			System.out.println("--------------------");
 			System.out.println(i+1+"- Numero: "+lista[i]);
 		}
+		
+		System.out.println("\nLa suma de los pares es "+suma);
+		System.out.println("\nEl producto de los impares es "+producto);
 	}
 
 }
