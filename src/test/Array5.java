226a0989bfd20da1c607494b072912acd4a12b87
beatrizcalzadillaso <beatriz.calzadillaso@elorrieta-errekamari.com>
2025-11-24 20:16:27 +0100

    exercise1 finished
    finished and working see later for improvements

--- src/test/Array5.java
+++ src/test/Array5.java
@@ -9,20 +9,20 @@ public class Array5 {
 
 	private static Scanner teclado = new Scanner(System.in);
 	private static int listaEdades[] = new int[20];
+	private static int mediaEntera=0;
  	
 	public static void main(String[] args) {
 		leerEdad();
 		calcularMedia(listaEdades);
-		for(int i=0; i<listaEdades.length; i++) {
-			System.out.println("--------------------");
-			System.out.println(i+1+"- Edad: "+listaEdades[i]);
-		}
-
+		imprimirEdades(listaEdades);
+				
 	}
+	
 	private static int[] leerEdad() {
 		
 		int valorEdad=0;
 		int cantidad= 0;
+		
 		
 		do{
 			boolean correcto= false;
@@ -52,7 +52,30 @@ public class Array5 {
 	}
 	
 	private static int calcularMedia(int[] lista) {
-		int mediaEntera=0;
+	
+		int suma=0;
+		
+		for (int i=0; i<lista.length; i++) {
+			suma = suma + lista[i];
+		}
+		
+		float media=suma/(lista.length);
+		mediaEntera = Math.round(media);
+		
+		// FALTA QUE IMPRIMA SOLO EL FLOAT MEDIA CON DOS CIFRAS DECIMALES, REVISAR Y AJUSTAR
+		//System.out.printf("\nLa media de edades es %.2f y se ha redondeado a %d", media, mediaEntera);
 		return mediaEntera;
 	}
+	
+	
+	private static void imprimirEdades(int[] lista) {
+	
+		for(int i=0; i<lista.length; i++) {
+			System.out.println("--------------------");
+			System.out.println(i+1+"- Edad: "+lista[i]);
+		}
+		
+		System.out.println("\nEl promedio de edades es "+mediaEntera);
+	}
+	
 }
---
+++ src/test/Ejercicio2_array.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * 
+ */
+public class Ejercicio2_array {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
