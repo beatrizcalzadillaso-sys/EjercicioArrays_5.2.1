16465de78a29d73136e05695f569bfa45ab3165a
Beatriz_Calzadilla <betty071194@gmail.com>
2025-11-26 14:22:01 +0100

    innate exercise 2
    only added names

---
+++ src/test/Ejercicio4_array.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * 
+ */
+public class Ejercicio4_array {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Alejandro Torres Maldonado
+
+		//Marina López Cevallos
+
+		//Javier Serrano Álvarez
+
+		//Claudia Ramos Villalba
+
+		//Rubén Ortega Benítez
+
+		//Sofía Delgado Carranza
+
+		//Héctor Morales Quintana
+
+		//Natalia Cabrera Soriano
+
+		//Diego Esteban Riquelme
+
+		//Laura Paredes Montenegro
+	}
+
+}
