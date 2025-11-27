63bf125d3cd7db956eae5c8c1a872c01b68fb1f4
beatrizcalzadillaso <beatriz.calzadillaso@elorrieta-errekamari.com>
2025-11-27 17:54:25 +0100

    sorte alphabet genrated
    populated array alphabet with 100 characters

--- src/test/Ejercicio5_array.java
+++ src/test/Ejercicio5_array.java
@@ -3,12 +3,14 @@
  */
 package test;
 
+import java.util.Random;
+
 /**
  * 
  */
 public class Ejercicio5_array {
 
-	private static int[] lotery= new int[100];
+	private static char[] lotery= new char[100];
 	private static int[] lotFrecuency= new int[26];
 	
 	public static void main(String[] args) {
@@ -21,14 +23,27 @@ public class Ejercicio5_array {
 	}
 
 	private static void generarSorteo(int numTiradas, int valorMin, int valorMax) {
-		
+		// Source - https://stackoverflow.com/a
+		// Posted by dogbane
+		// Retrieved 2025-11-27, License - CC BY-SA 2.5
+
+		for (int i=0; i<numTiradas; i++) {
+			Random r = new Random();
+			char c = (char)(r.nextInt(26) + 'a');
+			lotery[i]=c;
+			}
 	}
 	
-	private static void mostrarSorteo(int[] arraySorteo) {
-		
+	private static void mostrarSorteo(char[] arraySorteo) {
+		for (int i=0; i<arraySorteo.length; i++) {
+			if (i==15 ||i==30 ||i==45 ||i==60 ||i==75 ||i==90){
+				System.out.println("\n");
+			}			
+			System.out.printf(" %c ",arraySorteo[i]);
+		}
 	}
 	
-	private static void contarFrecuencias(int[] arraySorteo, int valorMin, int valorMax) {
+	private static void contarFrecuencias(char[] arraySorteo, int valorMin, int valorMax) {
 		
 	}
 	
