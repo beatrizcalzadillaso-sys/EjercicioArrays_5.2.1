0fd46bedd8e46c67f04cbf9d02b074ab6c6c69e9
Beatriz_Calzadilla <betty071194@gmail.com>
2025-11-27 12:18:38 +0100

    exercise 4 completed
    completed and working

--- src/test/Ejercicio4_array.java
+++ src/test/Ejercicio4_array.java
@@ -8,31 +8,48 @@ package test;
  */
 public class Ejercicio4_array {
 
-	/**
-	 * @param args
-	 */
+	private static String [] nombres= new String[10];
+	private static int[] notas= new int[10];
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		//Alejandro Torres Maldonado
-
-		//Marina López Cevallos
-
-		//Javier Serrano Álvarez
-
-		//Claudia Ramos Villalba
-
-		//Rubén Ortega Benítez
-
-		//Sofía Delgado Carranza
-
-		//Héctor Morales Quintana
-
-		//Natalia Cabrera Soriano
-
-		//Diego Esteban Riquelme
-
-		//Laura Paredes Montenegro
+		iniciarNombres(nombres);
+		iniciarNotas(notas);
+		imprimir(nombres, notas);
 	}
 
+	private static void iniciarNombres(String[] words) {
+		words[0]="Alejandro Torres Maldonado";
+		words[1]="Marina López Cevallos";
+		words[2]="Javier Serrano Álvarez";
+		words[3]="Claudia Ramos Villalba";
+		words[4]="Rubén Ortega Benítez";
+		words[5]="Sofía Delgado Carranza";
+		words[6]="Héctor Morales Quintana";
+		words[7]="Natalia Cabrera Soriano";
+		words[8]="Diego Esteban Riquelme";
+		words[9]="Laura Paredes Montenegro";
+			
+	}
+	private static void iniciarNotas(int[] numbers) {
+		int contador=0;
+		
+		
+		do {
+			int randomGrade= (int) Math.floor(Math.random() * 11 + 1); // (Math.random() * (max - min + 1)) + min)
+			if (randomGrade>=0 && randomGrade<=10) {
+				numbers[contador]=randomGrade;
+				contador++;
+			}
+			
+		}while(contador!=10);
+	}
+	
+	private static void imprimir(String[] words, int[] numbers) {
+		System.out.println("Las notas de los estudiantes son: ");
+		for (int i=0; i< words.length; i++) {
+			int index= i+1;
+			System.out.printf("Programacion: %-3d- %-28s || %d%n", index, words[i], numbers[i]);
+			
+		}
+	} 
 }
