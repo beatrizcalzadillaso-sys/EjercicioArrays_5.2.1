4c60c4e8b82e9c8583e7a9c92292ab2b6b3039b5
beatrizcalzadillaso <beatriz.calzadillaso@elorrieta-errekamari.com>
2025-11-27 17:36:15 +0100

    ex 5 framework
    created basic framework for ex 5

---
+++ src/test/Ejercicio5_array.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * 
+ */
+public class Ejercicio5_array {
+
+	private static int[] lotery= new int[100];
+	private static int[] lotFrecuency= new int[26];
+	
+	public static void main(String[] args) {
+		generarSorteo(100,1,100);
+		mostrarSorteo(lotery);
+		contarFrecuencias(lotery, 1, 100);
+		mostrarFrecuencias(lotFrecuency, 1, 100);
+		
+
+	}
+
+	private static void generarSorteo(int numTiradas, int valorMin, int valorMax) {
+		
+	}
+	
+	private static void mostrarSorteo(int[] arraySorteo) {
+		
+	}
+	
+	private static void contarFrecuencias(int[] arraySorteo, int valorMin, int valorMax) {
+		
+	}
+	
+	private static void mostrarFrecuencias(int[] arrayFrecuencias, int valorMin, int valorMax) {
+		
+	}
+	
+}
