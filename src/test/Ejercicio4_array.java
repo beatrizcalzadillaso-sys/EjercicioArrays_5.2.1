30e2bdab1df4009bb02a51cbf097359c8ef0c9d5
beatrizcalzadillaso <beatriz.calzadillaso@elorrieta-errekamari.com>
2025-12-02 16:35:37 +0100

    modified 4
    determined highest grade

--- src/test/Ejercicio4_array.java
+++ src/test/Ejercicio4_array.java
@@ -15,6 +15,7 @@ public class Ejercicio4_array {
 		iniciarNombres(nombres);
 		iniciarNotas(notas);
 		imprimir(nombres, notas);
+		calculoDatos(notas);
 	}
 
 	private static void iniciarNombres(String[] words) {
@@ -51,5 +52,45 @@ public class Ejercicio4_array {
 			System.out.printf("Programacion: %-3d- %-28s || %d%n", index, words[i], numbers[i]);
 			
 		}
-	} 
+	}
+	
+	private static void calculoDatos(int[] numbers) {
+		// ENCONTRAR MAYOR NOTA
+		int highest= numbers[numbers.length-1];
+		for (int i=0; i<numbers.length-1; i++) {
+			if (numbers[i]>highest) {
+				highest=numbers[i];
+				}
+			}
+		System.out.printf("\n La mayor nota es: %d%n%n",highest);
+		
+		// HAY MAS DE UNA PERSONA CON LA MAYOR NOTA?
+/*		
+  		int count= 0;
+		for(int i=0; i<numbers.length;i++) {
+			if (numbers[i]==highest) {
+				count++;
+				}
+			}
+		
+		// IMPRIMIR NOMBRE DE LA PERSONA/S CON MAYOR NOTA	
+		if (count>1) {
+				String [] elite= new String[count];
+			}
+		else {String [] elite= new String[1];}      */
+	
+		
+		String [] elite= new String[10];
+		
+		for (int i=0; i<numbers.length; i++) {
+			if (numbers[i]==highest) {
+				elite[i]=nombres[i];
+				} 
+			}
+		
+		for (int i=0; i<elite.length;i++) {
+			if (elite[i]!= null) {
+				System.out.println(elite[i]+" tiene la mayor nota de la clase");}
+			}
+	}
 }
