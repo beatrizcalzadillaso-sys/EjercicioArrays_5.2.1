b53d4e9503d33295ab903cbdaa787a9e95098633
beatrizcalzadillaso <beatriz.calzadillaso@elorrieta-errekamari.com>
2025-12-03 16:54:11 +0100

    finished 7
    finished ex7, multi 50%

--- src/test/Ejercicio7_array.java
+++ src/test/Ejercicio7_array.java
@@ -18,7 +18,7 @@ public class Ejercicio7_array {
 	}
 	
 	private static int[] cargarTabla(int n, int valorMin, int valorMax) {
-		for (int i= 0; i< n+1; i++){
+		for (int i= 0; i< n; i++){
 			int aleatorio= (int) Math.floor(Math.random() * (valorMax-valorMin +1) + valorMin); // (Math.random() * (max - min + 1)) + min)
 			table2[i]=aleatorio;
 			}
@@ -28,12 +28,24 @@ public class Ejercicio7_array {
 	private static void moverCerosFinal (int [] table) {
 		int iFix=0;
 		
+		// LLEVAR LOS ELEMENTOS DISTINTOS DE CERO A OTRA TABLA
 		for (int i=0; i<table.length; i++) {
 			if(table[i]!=0) {
 				tableFix[iFix]=table[i];
 				iFix++;
 			}
 		}
+		System.out.println("---Tabla aleatoria---");
+		for (int i=0; i<table.length;i++) {
+			System.out.printf("%d  ",table[i]);
+			}
+		System.out.println("\n");
+		
+		System.out.println("---Tabla con ceros separados---");
+		for (int i=0; i<tableFix.length;i++) {
+			System.out.printf("%d  ",tableFix[i]);
+		}
+		
 	}
 
 }
--- src/test/NotasMulti.java
+++ src/test/NotasMulti.java
@@ -20,7 +20,26 @@ public class NotasMulti {
 		iniciarNotas(nRedes);
 		iniciarNotas(nBd);
 		imprimir(estudiantes, nProg, nRedes, nBd);
-
+		// POR ESTUDIANTE
+		aveStudent(estudiantes, nProg, nRedes, nBd);
+		// POR ASIGNATURA
+		average("Programacion", nProg);
+		average("Redes", nRedes);
+		average("Bases de datos", nBd);
+		System.out.println("-------------------------------------");
+		highest("Programacion", nProg, estudiantes);
+		highest("Redes", nRedes, estudiantes);
+		highest("Bases de datos", nBd, estudiantes);
+		System.out.println("-------------------------------------");
+		lowest("Programacion", nProg, estudiantes);
+		lowest("Redes", nRedes, estudiantes);
+		lowest("Bases de datos", nBd, estudiantes);
+		System.out.println("-------------------------------------");
+		failed("Programacion", nProg);
+		failed("Redes", nRedes);
+		failed("Bases de datos", nBd);
+		System.out.println("-------------------------------------");
+		
 	}
 	
 	private static void iniciarNombres(String[] words) {
@@ -48,9 +67,70 @@ public class NotasMulti {
 	private static void imprimir(String[] words,  int[] grade1, int[] grade2, int[] grade3) {
 		System.out.println("Las notas de los estudiantes son: ");
 		for (int i=0; i< words.length; i++) {
-			System.out.printf("Nombre: %-28s || Programacion: %d || Redes: %d || Bases de Datos: %d%n", words[i], grade1[i], grade2[i], grade3[i]);
+			System.out.printf("Nombre: %-28s || Programacion: %d    || Redes: %d    || Bases de Datos: %d%n", words[i], grade1[i], grade2[i], grade3[i]);
 			
 		}
+		System.out.println("-------------------------------------");
 	}
+	
+	private static void aveStudent(String[] words,  int[] grade1, int[] grade2, int[] grade3) {
+		
+		int sum=0;
+		float ave=0;
+		
+		for (int i=0; i<grade1.length; i++) {
+			sum = grade1[i] + grade2[i]+ grade3[i];
+			ave = sum/3;
+			System.out.printf("El estudiante %-28s tuvo como nota media entre las tres asignaturas %.2f%n", words[i], ave);
+		}
+	}
+	
+	private static void average(String asignatura, int[] grade) {
+		int suma =0;
+		for (int i=0; i<grade.length;i++) {
+			suma = suma + grade[i];
+		}
+		float media =  suma/grade.length;
+		
+		System.out.printf("La asignatura %-15s tuvo como media %.1f%n", asignatura, media);
+		
+	}
+	
+	private static void highest(String asignatura, int[] grade, String[] words) {
+			
+		int highest= grade[grade.length-1];
+		int best =0;
+		for (int i=0; i<grade.length-1; i++) {
+			if (grade[i]>highest) {
+				highest=grade[i];
+				best=i;
+				}
+			}
+		System.out.printf("La asignatura %-15s tuvo como maximo %d , y fue el estudiante %-28s%n", asignatura, highest, words[best]);
+	}
+	
+	private static void lowest(String asignatura, int[] grade, String[] words) {
+		
+		int lowest= grade[grade.length-1];
+		int worst =0;
+		for (int i=0; i<grade.length; i++) {
+			if (grade[i]<lowest) {
+				lowest=grade[i];
+				worst=i;
+				}
+			}
+		System.out.printf("La asignatura %-15s tuvo como minimo %d, y fue el estudiante %-28s%n", asignatura, lowest, words[worst]);
+	}
+	
+	private static void failed(String asignatura, int[] grade) {
+		int amount=0;
+		for (int i=0; i<grade.length;i++) {
+			if (grade[i]<6) {
+				amount++;
+			}
+		}
+		System.out.printf("En la asignatura %-15s hubo %d suspensos con notas menores de 6\n", asignatura, amount);
+	}
+	
 
 }
