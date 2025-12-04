674e4e740b503603a9fea654b5e806ea42f40492
Beatriz Calzadilla Sosa <beatriz.calzadillaso@elorrieta-errekamari.com>
2025-12-04 14:11:01 +0100

    ex4
    nearly finished

--- src/test/Ejercicio4_array.java
+++ src/test/Ejercicio4_array.java
@@ -8,14 +8,15 @@ package test;
  */
 public class Ejercicio4_array {
 
-	private static String [] nombres= new String[10];
+	private static String [] estudiantes= new String[10];
 	private static int[] notas= new int[10];
 	
 	public static void main(String[] args) {
-		iniciarNombres(nombres);
+		iniciarNombres(estudiantes);
 		iniciarNotas(notas);
-		imprimir(nombres, notas);
-		calculoDatos(notas);
+		imprimir(estudiantes, notas);
+		System.out.println("-------------------------------------");
+		calculoDatos(notas, estudiantes);
 	}
 
 	private static void iniciarNombres(String[] words) {
@@ -54,7 +55,37 @@ public class Ejercicio4_array {
 		}
 	}
 	
-	private static void calculoDatos(int[] numbers) {
+	private static void calculoDatos(int[] numbers, String [] names) {
+		// MEDIA DE LAS NOTAS
+		int sum= 0;
+		for (int i=0; i<numbers.length;i++) {
+			sum = sum + numbers[i];
+		}
+		float average =  (float)sum/numbers.length;
+		
+		System.out.printf("La media de las notas en la clase fue %.1f%n", average);
+		System.out.println("-------------------------------------");
+		
+		// ALUMNOS CON NOTA MAYOR QUE LA MEDIA
+		int [] bestGrades= new int[10];
+		String [] bestNames= new String[10];
+		int indexBest=0;
+		
+		for(int i=0; i<numbers.length; i++) {
+			if (numbers[i]>=average) {
+				bestGrades[indexBest]=numbers[i];
+				bestNames[indexBest]=names[i];
+				indexBest++;
+			}
+		}
+		System.out.println("Los estudiantes con notas mayor que la media son: \n");
+		for(int i=0; i<bestGrades.length; i++) {
+			if(bestGrades[i]!=0) {
+				System.out.printf("%-28s : %d%n", bestNames[i], bestGrades[i]);
+			}
+		}
+		
+		System.out.println("-------------------------------------");
 		// ENCONTRAR MAYOR NOTA
 		int highest= numbers[numbers.length-1];
 		for (int i=0; i<numbers.length-1; i++) {
@@ -64,33 +95,52 @@ public class Ejercicio4_array {
 			}
 		System.out.printf("\n La mayor nota es: %d%n%n",highest);
 		
-		// HAY MAS DE UNA PERSONA CON LA MAYOR NOTA?
-/*		
-  		int count= 0;
-		for(int i=0; i<numbers.length;i++) {
-			if (numbers[i]==highest) {
-				count++;
-				}
-			}
-		
-		// IMPRIMIR NOMBRE DE LA PERSONA/S CON MAYOR NOTA	
-		if (count>1) {
-				String [] elite= new String[count];
-			}
-		else {String [] elite= new String[1];}      */
-	
+			// HAY MAS DE UNA PERSONA CON LA MAYOR NOTA?
 		
 		String [] elite= new String[10];
+		int indexElite=0;
 		
 		for (int i=0; i<numbers.length; i++) {
 			if (numbers[i]==highest) {
-				elite[i]=nombres[i];
+				elite[indexElite]=names[i];
+				indexElite++;
 				} 
 			}
-		
+			// IMPRIME LA LISTA DE PERSONAS QUE TIENEN LA MAYOR NOTA
 		for (int i=0; i<elite.length;i++) {
 			if (elite[i]!= null) {
-				System.out.println(elite[i]+" tiene la mayor nota de la clase");}
+				System.out.printf("%-28s tiene la mayor nota de la clase\n\n", elite[i]);}
 			}
+		
+			// IMPRIME LA ULTIMA PERSONA CON LA MAYOR NOTA
+		System.out.println(elite[indexElite-1]+" es la ultima persona consultada con la mayor nota");
+		
+		// ENCONTRAR MENOR NOTA
+		int lowest= numbers[0];
+		for (int i=0; i<numbers.length-1; i++) {
+			if (numbers[i]<lowest) {
+				lowest=numbers[i];
+				}
+			}
+		System.out.printf("\n La menor nota es: %d%n%n",lowest);
+		
+			// IMPRIMIR LA MENOR NOTA
+		String [] bottom= new String[10];
+		int indexBottom=0;
+		
+		for (int i=0; i<numbers.length; i++) {
+			if (numbers[i]==lowest) {
+				bottom[indexBottom]=names[i];
+				indexBottom++;
+				} 
+			}
+			// IMPRIME LA LISTA DE PERSONAS QUE TIENEN LA MAYOR NOTA
+		for (int i=0; i<bottom.length;i++) {
+			if (bottom[i]!= null) {
+				System.out.printf("%-28s tiene la menor nota de la clase\n\n", bottom[i]);}
+		}
+		// IMPRIME LA ULTIMA PERSONA CON LA menor NOTA
+		System.out.println(bottom[indexBottom-1]+" es la ultima persona consultada con la menor nota");
 	}
+	
 }
